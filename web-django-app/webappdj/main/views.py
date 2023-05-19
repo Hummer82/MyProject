e5a7ef90f3d16aa11edbf0c8f7b52aabd4f20900
Hummer82 <hummer.hx1@gmail.com>
2023-05-19 14:32:35 +0300

    Jinja и HTML

--- web-django-app/webappdj/main/views.py
+++ web-django-app/webappdj/main/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h4>Проверка работы</4>")
+    #return HttpResponse("<h4>Проверка работы</4>")
+    return render(request,"main/index.html")
 
 def about(request):
-    return HttpResponse("<h4>Про нас</4>")
+    #return HttpResponse("<h4>Про нас</4>")
+    return render(request,"main/about.html")
