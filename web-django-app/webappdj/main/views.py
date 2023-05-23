4c6673229df8e39a3eef5d844460b915492757bd
Hummer82 <hummer.hx1@gmail.com>
2023-05-23 11:21:23 +0300

    dj

--- web-django-app/webappdj/main/views.py
+++ web-django-app/webappdj/main/views.py
@@ -5,9 +5,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    data = {
+        'title':'Главная страница',
+        'values': ['Some','Hello'],
+        'obj': {
+            'car': 'BMW',
+            'age': 18,
+            'hobby': 'Football'
+        }
+    }
     #return HttpResponse("<h4>Проверка работы</4>")
-    return render(request,"main/index.html")
+    return render(request,"main/index.html",data)
 
 def about(request):
     #return HttpResponse("<h4>Про нас</4>")
     return render(request,"main/about.html")
+
+def contacts(request):
+    #return HttpResponse("<h4>Про нас</4>")
+    return render(request,"main/contacts.html")
