56ff7bed565de6f41a401a9bd420d8d3a893d22d
Hummer82 <hummer.hx1@gmail.com>
2023-05-24 12:27:13 +0300

    end dj

--- web-django-app/webappdj/news/views.py
+++ web-django-app/webappdj/news/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
     news = Articles.objects.all() # .order_by('-date')[:2] - выборка 2-х записей, отсортированных по дате
     return render(request,'news/news_home.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/detail_view.html'
+    context_object_name = 'article'
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news_delete.html'
+    context_object_name = 'article'
+    
 
 def create(request):
     error = ''
