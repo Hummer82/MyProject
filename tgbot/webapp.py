8a2f5a266d9ed3a88ef99385f2bfd1cc05fd73d1
Hummer82 <hummer.hx1@gmail.com>
2023-09-11 07:35:50 +0300

    tgbot

--- tgbot/webapp.py
+++ tgbot/webapp.py
@@ -5,6 +5,8 @@ from aiogram.types.web_app_info import WebAppInfo
 bot = Bot("6513613001:AAHZZ4GtQwRXpctL7L_KHasq1QRToDX-UY8")
 dp = Dispatcher(bot)
 
+myid = 0
+
 @dp.message_handler(commands=['web'])
 async def website(message: types.Message):
     await message.answer(f'{message}')
@@ -18,20 +20,22 @@ async def start(message: types.Message):
     markup.add(types.KeyboardButton('Вход', callback_data='login'))
     fn = message.chat.first_name if message.chat.first_name != None else ""
     ln = message.chat.last_name if message.chat.last_name != None else ""
-    await message.answer(f'{fn} {ln}, выберите действие',reply_markup=markup)
+    await message.answer(f'{fn} {ln}, выберите действие\n{message}',reply_markup=markup)
 
 @dp.callback_query_handler()
 async def sendmanager(call):
     if call.data == 'send_manager':
-        myid = call.message['from']['id']
-        await bot.send_message(6039681226,f'От: {myid}\n text', reply_to_message_id=myid)
+        global myid
+        myid = call.message.chat.id
+        await bot.send_message(6039681226,f'От:{call.message.from_user.first_name} {call.message.from_user.last_name} {myid}\n text')
     else:
         await call.message.answer(call)
     
 @dp.message_handler(content_types=['text','photo','video','voice'])
 async def mess(message: types.Message):
-    reply_id = message.reply_to_message['from']['id']
-    await bot.send_message(reply_id,'ooh')
+    global myid
+    #reply_id = message.chat.id
+    await bot.send_message(myid,message.text)
     # if int(message.reply_to_message['from']['id']) != int(message['from']['id']):
     #     await bot.send_message(message.reply_to_message['from']['id'], message.text)
     # else:
